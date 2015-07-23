da9d48472c54fdcc4950e8afe74897677e81dda4
Nate George <wordsforthewise@gmail.com>
2015-07-23 14:19:32 -0700

    added TCO data and put all data in a dict

--- manufacturing process data/plotArcing.py
+++ manufacturing process data/plotArcing.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 import pylab as plt
 from datetime import datetime
@@ -6,26 +7,32 @@ startTime = datetime.now()
     
 plt.style.use('dark_background')
 
-PCarc440 = pd.read_excel('C:/Users/nathan.george/Downloads/440 PC arcing.xlsx')
-PCarc450 = pd.read_excel('C:/Users/nathan.george/Downloads/450 PC arcing.xlsx')
-BEarc440 = pd.read_excel('C:/Users/nathan.george/Downloads/440 BE arcing.xlsx')
-BEarc450 = pd.read_excel('C:/Users/nathan.george/Downloads/450 BE arcing.xlsx')
+processes = ['BE','PC','TCO']
+runs = ['440','450']
+runData = {}
+
+for run in runs:
+    runData.setdefault(run,{})
+    for process in processes:
+        runData[run][process] = pd.read_excel('C:/Users/nathan.george/Downloads/' + run + ' ' + process + ' arcing.xlsx')
 
 loadTime = (datetime.now()-startTime).total_seconds()
 
 print 'took', loadTime, 'seconds to load, or', loadTime/60, 'minutes'
 
-for process in ['BE','PC']:
-    for key in eval(process + 'arc440').columns:
+for process in processes:
+    if not os.path.exists('Y:/Nate/git/nuvosun-python-lib/manufacturing process data/' + process):
+        os.mkdir('Y:/Nate/git/nuvosun-python-lib/manufacturing process data/' + process)
+    for key in runData[runs[0]][process].columns:
         if key!='DT' and key!='Down Web Pos':
-            ax = pd.DataFrame.plot(eval(process + 'arc440'), kind='scatter', x = 'Down Web Pos', y = key, label = '440', linewidth = 0, c = 'red')
-            pd.DataFrame.plot(eval(process + 'arc450'), kind='scatter', x = 'Down Web Pos', y = key, label = '450', ax = ax, linewidth = 0, c = 'white')
+            ax = pd.DataFrame.plot(runData[run[0]][process], kind='scatter', x = 'Down Web Pos', y = key, label = runs[0], linewidth = 0, c = 'red')
+            pd.DataFrame.plot(runData[run[1]][process], kind='scatter', x = 'Down Web Pos', y = key, label = runs[1], ax = ax, linewidth = 0, c = 'white')
             patches, labels = ax.get_legend_handles_labels()
             ax.legend(patches, labels, loc='best')
             plt.savefig('Y:/Nate/git/nuvosun-python-lib/manufacturing process data/' + process + '/' + key, edgecolor='none', bbox_inches = 'tight')
-            plt.close()
+            plt.close() 
         
 
 completeTime = (datetime.now()-startTime).total_seconds()
 
-print 'took', completeTime, 'seconds to load, or', completeTime/60, 'minutes'
+print 'took', completeTime, 'seconds to complete, or', completeTime/60, 'minutes'
